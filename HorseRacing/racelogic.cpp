5365b077c87250500a07016d64beb46398955614
socec <igorsocec@gmail.com>
2015-03-25 15:53:31 +0100

    race logic monitors standings and results

--- HorseRacing/racelogic.cpp
+++ HorseRacing/racelogic.cpp
@@ -6,9 +6,10 @@ RaceLogic::RaceLogic(float trackLength, int horseCount)
     finished = false;
     // initial camera position
     cameraPos = 0.0;
-    // initial horse positions
+    // initial horse positions and standings
     for (int i = 0; i < horseCount; i++) {
         horsePos.insert(horsePos.end(), 0.0);
+        standings.insert(standings.end(), 0);
     }
 }
 
@@ -16,15 +17,47 @@ void RaceLogic::nexTick()
 {
     // advance camera at constant speed
     cameraPos += baseSpeed;
+
     // advance each horse at random speed
     for (int i = 0; i < horsePos.size(); i++) {
         horsePos.at(i) += (baseSpeed - 1) + rand() % 3;
     }
 
+    updateStandings();
     // check if race is finished
-    int finishedCount = 0;
-    for (int i = 0; i < horsePos.size(); i++) {
-        if (horsePos.at(i) >= trackLength) finishedCount++;
+    if (!results.empty()) {
+        finished = (results.size() == horsePos.size());
     }
-    finished = (finishedCount == horsePos.size());
+}
+
+void RaceLogic::updateStandings()
+{
+    // helping structure for sorting standings (sort indices based on current position)
+    struct Info {
+        int index;
+        float position;
+    };
+    std::vector<Info> horseInfo;
+
+    // collect information
+    for (int i = 0; i < horsePos.size(); i++) {
+        Info info = {i, horsePos.at(i)};
+        horseInfo.insert(horseInfo.end(), info);
+    }
+
+    // sort standings, using lambda function to overload default sort operator
+    std::sort(horseInfo.begin(), horseInfo.end(),
+              [] (Info info1, Info info2) { return info1.position > info2.position; });
+
+    // update current standings after sorting
+    for (int i = 0; i < horseInfo.size(); i++) {
+        Info info = horseInfo.at(i);
+        standings.at(i) = info.index;
+        // add the horse that finished the race to final results
+        if (info.position >= trackLength)
+            // add horse if not already in the final results
+            if (std::find(results.begin(), results.end(), info.index) == results.end()) {
+                results.insert(results.end(), info.index);
+            }
+    }
 }
--- HorseRacing/racelogic.h
+++ HorseRacing/racelogic.h
@@ -15,6 +15,8 @@ public:
     std::vector<float> getHorsePos() { return horsePos; }
     int getHorseCount() { return horsePos.size(); }
     float getCameraPos() { return cameraPos; }
+    std::vector<int> getStandings() { return standings; }
+    std::vector<int> getResults() { return results; }
 
     bool raceFinished() { return finished; }
     void nexTick();
@@ -23,8 +25,11 @@ private:
     float trackLength;
     std::vector<float> horsePos;
     float cameraPos;
+    std::vector<int> standings, results;
     bool finished;
     float baseSpeed = BASE_SPEED;
+
+    void updateStandings();
 };
 
 #endif // RACELOGIC_H
